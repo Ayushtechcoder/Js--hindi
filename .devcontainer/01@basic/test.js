a16a4221a5ade804a46a7589504580343e9f0446
Ayushtechcoder <aayushh121@gmail.com>
2024-05-13 13:17:03 +0000

    config file directory

--- .devcontainer/01@basic/test.js
+++ .devcontainer/01@basic/test.js
@@ -1,1 +1,8 @@
-console.log("ayush")
+const accountID = 34456
+let accountEmail = "ayus@gmail.com"
+var accountPassword = "8283846021"
+accountCity = "palampur"
+let accountStates;
+
+
+console.table([accountID, accountEmail, accountPassword, accountcity])

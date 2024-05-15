a33adba53f116a3184f964931beed376fe726aa4
Ayushtechcoder <aayushh121@gmail.com>
2024-05-15 05:57:25 +0000

    data types

--- .devcontainer/01@basic/1.js
+++ .devcontainer/01@basic/1.js
@@ -6,3 +6,22 @@ console.log(3+
 )// code readability should be high
 console.log("ayush")
 
+
+let name = " ayush"
+let age =   " 20"
+let isLoggedIn = "false"
+let state = ""
+
+
+
+// number => 2 to power 53
+// bigint
+// string =>""
+// boolean =>true/false
+// undefined =>
+// null =>standalone value
+// symbol => unique
+
+
+// object
+ console.log( typeof"ayush")
